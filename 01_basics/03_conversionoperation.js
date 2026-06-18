60baf52db34e3cd2663a6c155e06fb2647f1bcdc
smaransiwakoti-debug <282847084+smaransiwakoti-debug@users.noreply.github.com>
2026-06-18 09:51:01 +0000

    conversision confusion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+let isLoggedIn = "smaran"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=>false
+// "" => false
+// " smaran " => true
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
